98566595b51a0b10ea965373d59eaec2d0c4a127
Daniel <DanielTarek3110@gmail.com>
2023-09-18 11:16:52 +0300

    fix request entity

--- src/main/java/com/example/otcsapi/Services/AuthenticationService.java
+++ src/main/java/com/example/otcsapi/Services/AuthenticationService.java
@@ -3,20 +3,24 @@ package com.example.otcsapi.Services;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class AuthenticationService {
 
     @Value("${OTCS_BASE_URL}")
     private String OTCS_BASE_URL ;
 
-    public RequestEntity<String> authenticate(String username , String password){
+    public String authenticate(String username , String password){
         String url= OTCS_BASE_URL + "v1/auth";
-        RequestEntity<String> response = restTemplate.exchange(
-            url,
-            HttpMethod,
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
 
-
-        );
-        return response;
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return responseEntity.getBody();
+        } else {
+            throw new RuntimeException("Failed to fetch data from API: " + responseEntity.getStatusCode());
+        }
     }
 }
+
