d045f6d3c2b9f0411e1a275116a16a30cfcf118f
Daniel <DanielTarek3110@gmail.com>
2023-09-18 09:49:22 +0300

    Initialize spring boot

---
+++ src/test/java/com/example/otcsapi/OtcsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otcsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtcsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
