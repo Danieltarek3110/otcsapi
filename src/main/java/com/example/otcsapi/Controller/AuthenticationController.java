46f7c239587a81fd784a6184be45a0461153e8bc
Daniel <DanielTarek3110@gmail.com>
2023-09-18 14:23:58 +0300

    Authentication API works using User class

--- src/main/java/com/example/otcsapi/Controller/AuthenticationController.java
+++ src/main/java/com/example/otcsapi/Controller/AuthenticationController.java
@@ -1,19 +1,13 @@
 package com.example.otcsapi.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.otcsapi.Services.AuthenticationService;
-
-import javax.print.attribute.standard.Media;
-
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import com.example.otcsapi.Models.User;
 
 
 @RestController
@@ -24,9 +18,9 @@ public class AuthenticationController {
     private AuthenticationService AuthService ;
 
     @PostMapping(value={"" , "/"} , produces = MediaType.APPLICATION_JSON_VALUE)
-    public String getTicket(@RequestParam String username , @RequestParam String password) {
+    public String getTicket(@RequestBody User usr) {
         
-       String response =  AuthService.authenticate(username, password);
+       String response =  AuthService.authenticate(usr.getUsername() , usr.getPassword());
 
         return response;
     }
---
+++ src/main/java/com/example/otcsapi/Services/UploadService.java
@@ -0,0 +1,8 @@
+package com.example.otcsapi.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UploadService {
+    
+}
